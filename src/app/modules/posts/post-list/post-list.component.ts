4a99490799cdc8dabd3df1db1b814f3fe18a00ae
Carlos Benavides <1992cab@gmail.com>
2019-08-10 00:00:00 -0500

    chore: added post list

---
+++ src/app/modules/posts/post-list/post-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post-service/post.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.scss']
+})
+export class PostListComponent implements OnInit {
+
+  posts: any = [];
+
+  constructor(private postService: PostService) { }
+
+  ngOnInit() {
+    this.postService.getAll().subscribe(res => {
+      this.posts = res;
+    }, err => console.error(err));
+  }
+}
