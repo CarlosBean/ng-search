b1abbf8f0d533b838e51a315020b38036920b7dd
Carlos Benavides <1992cab@gmail.com>
2019-08-12 16:38:24 -0500

    chore: added dependencies

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { TableComponent } from './table/table.component';
 import { MaterialModule } from './material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ModalComponent } from './modal/modal.component';
+import { FormInputComponent } from './form-input/form-input.component';
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+import { DynamicFieldDirective } from './dynamic-field/dynamic-field.directive';
+import { NO_ERRORS_SCHEMA } from '@angular/compiler/src/core';
 
 @NgModule({
     declarations: [
-        TableComponent
+        TableComponent,
+        ModalComponent,
+        FormInputComponent,
+        DynamicFormComponent,
+        DynamicFieldDirective
     ],
     imports: [
         CommonModule,
@@ -18,19 +27,24 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     ],
     exports: [
         TableComponent,
+        ModalComponent,
+        DynamicFormComponent,
         CommonModule,
         HttpClientModule,
         FormsModule,
         ReactiveFormsModule,
         MaterialModule
     ],
+    entryComponents: [
+        FormInputComponent,
+    ],
     providers: [],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SharedModule {
-    /*  static forRoot() {
-         return {
-             ngModule: SharedModule
-         };
-     } */
+    static forRoot() {
+        return {
+            ngModule: SharedModule
+        };
+    }
 }
