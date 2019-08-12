6992ca61fd3effa9083ca442d67fe5bc2f696d02
Carlos Benavides <1992cab@gmail.com>
2019-08-12 16:37:58 -0500

    chore: added update modal

--- src/app/modules/users/user-list/user-list.component.ts
+++ src/app/modules/users/user-list/user-list.component.ts
@@ -9,13 +9,36 @@ import { UserService } from '../user-service/user.service';
 export class UserListComponent implements OnInit {
 
   users = [];
-  columns = ['status', 'username', 'name', 'email', 'website', 'action'];
+  columns = ['id', 'status', 'username', 'name', 'email', 'website', 'action'];
+  openModal = false;
+  objectId = null;
+
+  mainSettings = {
+    title: 'Create an User',
+    footer: 'This component will be for create and update.'
+  };
 
   constructor(private userService: UserService) { }
 
   ngOnInit() {
     this.userService.getAll().subscribe(res => {
       this.users = res;
+    }, err => console.error(err));
+  }
+
+  getTableAction(event: any) {
+    if (event.action === 'edit') {
+      this.openModal = true;
+      this.objectId = event.data;
+      this.mainSettings.title = `Update user ${this.objectId}`;
+    } else if (event.action === 'delete') {
+      this.delete(event.data);
+    }
+  }
+
+  delete(id: any) {
+    this.userService.delete(id).subscribe(res => {
+      alert('DELETE SUCCESS');
     }, err => console.error(err));
   }
 }
