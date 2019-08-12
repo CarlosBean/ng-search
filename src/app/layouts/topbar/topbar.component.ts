698b0f590bacc79f13186ce971ed9bf14dbdae13
Carlos Benavides <1992cab@gmail.com>
2019-08-12 16:34:55 -0500

    chore: added custom placeholder by module

--- src/app/layouts/topbar/topbar.component.ts
+++ src/app/layouts/topbar/topbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { LocalStorage } from 'ngx-webstorage';
+import { LocalStorage, LocalStorageService } from 'ngx-webstorage';
 
 @Component({
   selector: 'app-topbar',
@@ -9,10 +9,24 @@ import { LocalStorage } from 'ngx-webstorage';
 export class TopbarComponent implements OnInit {
 
   @LocalStorage() searchText: string;
+  placeholder: string;
 
-  constructor() { }
+  constructor(private localStorage: LocalStorageService) { }
 
   ngOnInit() {
+    this.localStorage.observe('searchPlaceholder').subscribe((columns: string[]) => {
+      const aux = [...columns];
+      this.setPlaceholder(aux);
+    });
   }
 
+  setPlaceholder(columns: string[]) {
+    let text = 'Search by ';
+    if (columns.length > 3) {
+      text += `${columns.splice(0, 3).join(', ')}, etc...`;
+    } else {
+      text += `${columns.join(', ')}`;
+    }
+    this.placeholder = text;
+  }
 }
