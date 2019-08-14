5564660ff87c63d164f5aadcaa1fb0b6a9c7b8a6
Carlos Benavides <1992cab@gmail.com>
2019-08-13 22:08:53 -0500

    chore: modified template structure

--- src/app/modules/posts/post-list/post-list.component.ts
+++ src/app/modules/posts/post-list/post-list.component.ts
@@ -3,19 +3,54 @@ import { PostService } from '../post-service/post.service';
 
 @Component({
   selector: 'app-post-list',
-  templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.scss']
+  template: `
+    <app-post-update
+      [objectId]="objectId"
+      (action)="openModal = $event"
+      *ngIf="openModal">
+    </app-post-update>
+
+    <app-table
+      [elementData]="elementData"
+      [displayedColumns]="columns"
+      (action)="getTableAction($event)">
+    </app-table>
+  `,
 })
 export class PostListComponent implements OnInit {
 
-  posts: any = [];
-  columns = ['id', 'userId', 'title', 'body'];
+  elementData: any = [];
+  columns = ['id', 'title', 'body', 'action'];
+  openModal = false;
+  objectId = null;
 
   constructor(private postService: PostService) { }
 
   ngOnInit() {
     this.postService.getAll().subscribe(res => {
-      this.posts = res;
+      this.elementData = res;
+    }, err => console.error(err));
+  }
+
+  getTableAction(event: any) {
+    switch (event.action) {
+      case 'create':
+        this.openModal = true;
+        this.objectId = null;
+        break;
+      case 'edit':
+        this.openModal = true;
+        this.objectId = event.data;
+        break;
+      case 'delete':
+        this.delete(event.data);
+        break;
+    }
+  }
+
+  delete(id: any) {
+    this.postService.delete(id).subscribe(res => {
+      alert('DELETE SUCCESS');
     }, err => console.error(err));
   }
 }
