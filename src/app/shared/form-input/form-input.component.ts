c432227bdb124b62c03476d6f89208bd5f271281
Carlos Benavides <1992cab@gmail.com>
2019-10-13 21:31:03 -0500

    chore: added css util classes

--- src/app/shared/form-input/form-input.component.ts
+++ src/app/shared/form-input/form-input.component.ts
@@ -5,26 +5,25 @@ import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form-input',
-  styleUrls: ['./form-input.component.scss'],
   template: `
-  <mat-form-field [formGroup]="group" class="full-width">
-    <mat-label>{{ config.label }}</mat-label>
-    <input matInput [placeholder]="config.placeholder"
-    [formControlName]="config.name">
+    <mat-form-field [formGroup]="group" class="w-100">
+      <mat-label>{{ config.label }}</mat-label>
+      <input matInput [placeholder]="config.placeholder"
+      [formControlName]="config.name">
 
-    <mat-error *ngIf="group.get(config.name).errors?.required">
-      {{ config.label }} is <strong>required</strong>
-    </mat-error>
+      <mat-error *ngIf="group.get(config.name).errors?.required">
+        {{ config.label }} is <strong>required</strong>
+      </mat-error>
 
-    <mat-error *ngIf="group.get(config.name).errors?.email">
-      Invalid email address
-    </mat-error>
+      <mat-error *ngIf="group.get(config.name).errors?.email">
+        Invalid email address
+      </mat-error>
 
-    <mat-error *ngIf="group.get(config.name).errors?.minlength">
-      {{ config.label }} must have at least 4 characters
-    </mat-error>
+      <mat-error *ngIf="group.get(config.name).errors?.minlength">
+        {{ config.label }} must have at least 4 characters
+      </mat-error>
 
-  </mat-form-field>
+    </mat-form-field>
   `
 })
 export class FormInputComponent implements Field {
