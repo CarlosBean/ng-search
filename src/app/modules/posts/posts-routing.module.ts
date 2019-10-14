48278e6d649282ce241d6b1a7d7614c5c4579717
Carlos Benavides <1992cab@gmail.com>
2019-10-13 21:26:51 -0500

    chore: added outlet and resolver on post route

--- src/app/modules/posts/posts-routing.module.ts
+++ src/app/modules/posts/posts-routing.module.ts
@@ -1,14 +1,42 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule, Injectable } from '@angular/core';
+import { Routes, RouterModule, Resolve, ActivatedRouteSnapshot } from '@angular/router';
 
 import { PostListComponent } from './post-list/post-list.component';
+import { PostUpdateComponent } from './post-update/post-update.component';
+import { PostService } from './post-service/post.service';
+import { Observable, of } from 'rxjs';
+import { Post, IPost } from 'src/app/models/post.model';
 
+@Injectable({ providedIn: 'root' })
+export class PostResolve implements Resolve<IPost> {
+    constructor(private service: PostService) { }
+
+    resolve(route: ActivatedRouteSnapshot): Observable<IPost> {
+        const id = route.params.id ? route.params.id : null;
+        return id ? this.service.getById(id) : of(new Post());
+    }
+}
 
 const routes: Routes = [
     {
         path: '',
-        component: PostListComponent
-    }
+        component: PostListComponent,
+        data: { title: 'Posts List' }
+    },
+    {
+        path: 'new',
+        component: PostUpdateComponent,
+        resolve: { post: PostResolve },
+        outlet: 'popup',
+        data: { title: 'New Post' }
+    },
+    {
+        path: 'update/:id',
+        component: PostUpdateComponent,
+        resolve: { post: PostResolve },
+        outlet: 'popup',
+        data: { title: 'Update Post' }
+    },
 ];
 
 @NgModule({
