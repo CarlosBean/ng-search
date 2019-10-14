1a4abc27a8e9f29eb40711925baad18c3712e64a
Carlos Benavides <1992cab@gmail.com>
2019-10-13 21:30:24 -0500

    chore: added confirm setting feature

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ContentChild, TemplateRef, Input, HostListener, Output, EventEmitter } from '@angular/core';
+import { Component, ContentChild, TemplateRef, Input, HostListener, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertBoxService } from '../alert-box/alert-box.service';
 
 export interface IModalButton {
   name: string;
@@ -10,6 +12,7 @@ export interface IModalSettings {
   title: string;
   footer: string;
   buttons: IModalButton[];
+  confirm?: boolean;
 }
 
 @Component({
@@ -18,24 +21,32 @@ export interface IModalSettings {
   styleUrls: ['./modal.component.scss']
 })
 
-export class ModalComponent implements OnInit {
+export class ModalComponent {
 
   @Input() settings: IModalSettings;
   @Output() action = new EventEmitter<any>();
   @ContentChild(TemplateRef, { static: false }) template: TemplateRef<any>;
 
-  constructor() { }
+  constructor(private router: Router, private alertBox: AlertBoxService) { }
 
   @HostListener('document:keydown.escape', ['$event'])
   onKeydownHandler(event: KeyboardEvent) {
-    this.action.emit(false);
+    this.closeModal();
   }
 
-  ngOnInit() {
+  getModuleName(): string {
+    return this.router.url.split('/')[1];
   }
 
   closeModal() {
-    this.action.emit(false);
+    if (this.settings.confirm) {
+      this.alertBox.show('Are you sure to exit?', 'Your changes will be deleted on exit.');
+      this.alertBox.afterClosed().subscribe(accept =>
+        accept && this.router.navigate(['/', this.getModuleName(), { outlets: { popup: null } }])
+      );
+    } else {
+      this.router.navigate(['/', this.getModuleName(), { outlets: { popup: null } }]);
+    }
   }
 
   getButtonStyle(button: IModalButton): string {
