d38a352f4fbc004f31f183cde5a4e0f92aab23f9
Carlos Benavides <1992cab@gmail.com>
2019-10-13 21:24:14 -0500

    chore: added page title dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivationEnd, Router } from '@angular/router';
 import { LocalStorageService } from 'ngx-webstorage';
+import { Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +11,27 @@ import { LocalStorageService } from 'ngx-webstorage';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-search-table';
 
-  constructor(private localStorage: LocalStorageService) { }
+  constructor(
+    private router: Router,
+    private localStorage: LocalStorageService,
+    private pageTitle: Title
+  ) {
+    this.getRouteData().subscribe(data => {
+      const title = data.title || 'Undefined Title';
+      this.pageTitle.setTitle(title);
+    });
+  }
 
   onActivate() {
     this.localStorage.clear('searchText');
+  }
+
+  getRouteData(): Observable<any> {
+    return this.router.events.pipe(
+      filter(event => event instanceof ActivationEnd),
+      filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+      map((event: ActivationEnd) => event.snapshot.data)
+    );
   }
 }
