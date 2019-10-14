3885ce333ec5a21e8f6fb3664375a7f7c571995d
Carlos Benavides <1992cab@gmail.com>
2019-10-13 21:23:49 -0500

    style: remove comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
   {
     path: 'users',
-    // loadChildren: './modules/users/users.module#UsersModule'
     loadChildren: () => import('src/app/modules/users/users.module').then(m => m.UsersModule)
   },
   {
     path: 'posts',
-    // loadChildren: './modules/posts/posts.module#PostsModule'
     loadChildren: () => import('./modules/posts/posts.module').then(m => m.PostsModule)
   },
   {
     path: 'comments',
-    // loadChildren: './modules/comments/comments.module#CommentsModule'
     loadChildren: () => import('./modules/comments/comments.module').then(m => m.CommentsModule)
   }
 ];
